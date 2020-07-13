8d15a652ca260e4ca5e2693f5868d29b33386abd
Jordan Hunt <jordanhunt@fb.com>
2020-07-13 13:53:35 -0700

    "Added bottom navigation bar and replaced action bar with a toolbar"

--- app/src/main/java/com/example/collegeauction/MainActivity.java
+++ app/src/main/java/com/example/collegeauction/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.collegeauction;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.collegeauction.LoginFragments.LoginFragment;
 import com.parse.ParseUser;
@@ -14,5 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+        // Sets the toolbar text to nothing
+        getSupportActionBar().setTitle("");
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
