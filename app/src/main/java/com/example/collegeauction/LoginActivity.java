98494ea15a14b4f0a936e4514b54e1b424ee0493
Jordan Hunt <jordanhunt@fb.com>
2020-07-13 11:00:02 -0700

    new updated project

---
+++ app/src/main/java/com/example/collegeauction/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.collegeauction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Handles the login for users
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
